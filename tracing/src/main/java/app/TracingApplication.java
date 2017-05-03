d973b529dc38fce374c57f922a8df045dbb980f0
borism <boris.marn@gmail.com>
2017-05-03 19:54:28 +0200

    added tracing POC that doesnt yet work

--- tracing/src/main/java/app/TracingApplication.java
+++ tracing/src/main/java/app/TracingApplication.java
@@ -3,21 +3,17 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * Created by bm on 29.03.2017.
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableTurbine
-@EnableHystrixDashboard
-public class MonitoringApplication {
+public class TracingApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(MonitoringApplication.class, args);
+		SpringApplication.run(TracingApplication.class, args);
 	}
 
 }
